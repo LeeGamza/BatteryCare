353bc708963d2e75eb782779031f84626e82a076
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2024-11-28 16:13:55 +0900

    <BK-1128_2>

--- Back_2/app.js
+++ Back_2/app.js
@@ -1,8 +1,6 @@
 ﻿const noble = require('@abandonware/noble');
-<<<<<<< Updated upstream
 const { saveData, connectToMongoDB } = require('./database/mongoService');
 const { parseBmsData } = require('./utils/parser');
-=======
 const express = require('express');
 const { saveLowData, connectToMongoDB, saveCycleAndImbalance } = require('./database/mongoService');
 const { parseBmsData } = require('./utils/parser');
@@ -10,17 +8,12 @@ const { calculateCycle } = require('./utils/cycleCalculator');
 
 const app = express();
 const PORT = 3000; // Express 서버 포트 번호
->>>>>>> Stashed changes
-
 // MongoDB 연결
 connectToMongoDB();
 
-<<<<<<< Updated upstream
 // 마지막으로 처리된 데이터 타임스탬프
 let lastProcessedTimestamp = 0;
 
-=======
->>>>>>> Stashed changes
 noble.on('stateChange', async (state) => {
     if (state === 'poweredOn') {
         console.log('Starting BLE scanning...');
@@ -70,8 +63,6 @@ noble.on('discover', async (peripheral) => {
         }
     }
 });
-<<<<<<< Updated upstream
-=======
 
 app.get('/api/cycleCount', async (req, res) => {
     try {
@@ -90,4 +81,3 @@ app.get('/api/cycleCount', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
->>>>>>> Stashed changes
